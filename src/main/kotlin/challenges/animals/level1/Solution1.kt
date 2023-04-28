3400010ba502ae60be110e3bf7ea0d77207e66fb
hamthelegend <hamuelxd@gmail.com>
2023-04-29 02:32:16 +0800

    Completed [Solution1.kt] and [Solution3.kt]

--- src/main/kotlin/challenges/animals/level1/Solution1.kt
+++ src/main/kotlin/challenges/animals/level1/Solution1.kt
@@ -1,5 +1,22 @@
 package challenges.animals.level1
 
 fun main() {
+    print("Number of animals: ")
+    val animalCount = readln().toInt()
 
+    for (i in 1..animalCount) {
+        println("\nAnimal $i of $animalCount")
+
+        print("Cat or dog: ")
+        val animal = readln().lowercase()
+
+        print("Name: ")
+        val name = readln()
+
+        when (animal) {
+            "cat" -> println("$name meowed")
+            "dog" -> println("$name barked")
+            else -> println("$name talked")
+        }
+    }
 }
--- src/main/kotlin/challenges/animals/level3/Solution3.kt
+++ src/main/kotlin/challenges/animals/level3/Solution3.kt
@@ -1,10 +1,6 @@
 package challenges.animals.level3
 
-// Starter code
-// Uncomment before starting
 // Output could vary from inherent randomness
-
-/*
 fun main() {
     println(getRandomCatSound()) // meowed
     println(getRandomDogSound()) // barked
@@ -13,4 +9,19 @@ fun main() {
     makeAnimalSound("cow", "Herb") // Herb talked
     makeAnimalSound(name = "Ert") // Ert talked
 }
- */
+
+fun getRandomCatSound() = listOf("meowed", "hissed", "purred").random()
+
+fun getRandomDogSound() = listOf("barked", "growled").random()
+
+fun makeAnimalSound(animal: String = "", name: String) {
+    println(
+        "$name ${
+            when (animal) {
+                "cat" -> getRandomCatSound()
+                "dog" -> getRandomDogSound()
+                else -> "talked"
+            }
+        }"
+    )
+}
