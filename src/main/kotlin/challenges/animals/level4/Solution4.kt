5ff27e0d79c89f6597d41fdf468a2b21c228e6e9
David <erosval126@gmail.com>
2023-04-30 04:45:05 -0700

    Updated Solution 4

---
+++ src/main/kotlin/challenges/animals/level4/Solution4.kt
@@ -0,0 +1,42 @@
+package challenges.animals.level4
+
+class AnimalSpecies(val name: String, val sounds : List<String>)
+
+class Animal(private val species : AnimalSpecies, private val name: String, private var isAlive : Boolean = true){
+
+   fun makeSound() {
+       if(isAlive) {
+           println("$name ,${species.sounds.random()}")
+       }
+       else{
+           println("$name is already dead")
+       }
+   }
+
+     fun kill(){
+        if(!isAlive){
+            println("$name is already dead")
+        }
+        else{
+            println("Your ${species.name} $name died")
+        }
+
+    }
+
+}
+fun main(){
+
+    val cat = AnimalSpecies("Cat", listOf("meowed","purred","hissed"))
+    val dog  = AnimalSpecies("Dog", listOf("barked","whimpered","howled"))
+
+    val animal1 = Animal(cat,"Me-mow",true)
+    val animal2 = Animal(dog,"Jake",true)
+    val animal3 = Animal(dog,"Typhon",false)
+
+    animal1.kill()
+    animal3.kill()
+    animal1.makeSound()
+    animal2.makeSound()
+    animal3.makeSound()
+
+}
